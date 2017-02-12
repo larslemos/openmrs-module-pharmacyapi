89868a38c6a06a794c9aef4ec972b24de434ae20
Stélio Moiane <steliomo@gmail.com>
2017-02-13 00:56:01 +0200

    update the prescription set and include the dispensation encounter

--- api/src/main/java/org/openmrs/module/pharmacyapi/api/util/MappedEncounters.java
+++ api/src/main/java/org/openmrs/module/pharmacyapi/api/util/MappedEncounters.java
@@ -12,7 +12,7 @@ public class MappedEncounters {
 	
 	public static final String DEFAULT_ENCONTER_ROLE = "a0b03050-c99b-11e0-9572-0800200c9a66";
 	
-	public static final String DISPENSATION_ENCOUNTER_TYPE = "d82c441d-72cb-440f-b007-d3105ca7f58a";
+	public static final String DISPENSATION_ENCOUNTER_TYPE = "18fd49b7-6c2b-4604-88db-b3eb5b3a6d5f";
 	
 	public static final String FILA_ENCOUNTER_TYPE = "e279133c-1d5f-11e0-b929-000c29ad1d07";
 }
